3b89dbdbf2a5abc82528b0f3a15abdd29cbfee83
chalwe mwansa <chalwemwansa23@gmail.com>
2024-06-16 18:37:15 +0200

    added all the required files and done with task 0

---
+++ README.md
@@ -0,0 +1,8 @@
+this folder contains an implementation of a a file system where you can upload and view files.
+capabilities are:
+  user authentication
+  listing all files
+  uploading new files
+  changing file permissions
+  viewing a file
+  generating thumbnails for images
---
+++ utils/redis.js
@@ -0,0 +1,67 @@
+// module contains a redis class that has the functions set a key,
+// get a key as well as deleting a key from redis
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient()
+      .on('error', (err) => {
+        console.log(err);
+      });
+    (async () => {
+      await client.connect();
+    })();
+  }
+
+  // function that checks if redis has been connected successfully
+  // and returns true if yes else it returns false
+  isAlive() {
+    return (this.client.connected);
+  }
+
+  // the asynchronous get method that returns a value for a key
+  // passed as an argument
+  async get (key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, result) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(result);
+        };
+      });
+    });
+  }
+
+  // method that sets a value for a key for a specified period of time
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, result) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        } else {
+          return resolve(result);
+        };
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, result) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        } else {
+          return resolve(result);
+        };
+      });
+    });
+  }
+}
+
+// create an instance of redis client and export it
+const redisClient = new RedisClient();
+module.exports = redisClient;
